805adc0e1a2eb9c63e0696ba7dcbefe0b8f64b8a
schatradi <schatradi@SCHATRADI-3W10.tfanet.org>
2021-02-04 11:19:07 -0500

    Fix SonarErrors fo TaxInfomationSection

--- TGLAutomation/src/test/java/org/tfa/tgl/tests/TaxInformationSectionTest.java
+++ TGLAutomation/src/test/java/org/tfa/tgl/tests/TaxInformationSectionTest.java
@@ -1,6 +1,5 @@
 package org.tfa.tgl.tests;
 
-import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.tfa.framework.core.BaseTestMethods;
@@ -8,37 +7,28 @@ import org.tfa.tgl.pages.LoginPageTgl;
 import org.tfa.tgl.pages.TaxInformationSection;
 import org.tfa.tgl.utilities.web.TGLConstants;
 
-public class TaxInformationSectionTest extends BaseTestMethods{
+public class TaxInformationSectionTest extends BaseTestMethods {
 
-	LoginPageTgl loginpage;
-	Logger log=Logger.getLogger("rootLogger");
-	
 	@Test
-	public void tgl104ValidateTaxlInformationSection() throws Exception	{	
-		TaxInformationSection taxinfosection= new TaxInformationSection();
+	public void tgl104ValidateTaxlInformationSection() {
 		boolean result;
-		try{			
-			loginpage = new LoginPageTgl();
-			
-			// Below Assert ensures success login
-			result=loginpage.enterLoginInfo();
-			Assert.assertTrue(result, "EnterloginInfo method failed");	
-			
-			// Below Assert ensures that searched user has additional information section showing
-			Assert.assertTrue(taxinfosection.verifyTaxInformationSection(), "verifyTaxInformation method failed");	
-			Assert.assertTrue(taxinfosection.verifyHelpLinks(),"VerifyHelpLinks method failed");
-		}catch(Exception e){
-			Assert.fail();	
-			log.error(e);
-		}finally{
-			log.info(this.getClass().getEnclosingMethod() + " Test Execution Completion - Success!");
-		}
+		TaxInformationSection taxinfosection = new TaxInformationSection();
+
+		LoginPageTgl loginpage = new LoginPageTgl();
+
+		// Below Assert ensures success login
+		result = loginpage.enterLoginInfo();
+		Assert.assertTrue(result, "EnterloginInfo method failed");
+
+		// Below Assert ensures that searched user has additional information section
+		Assert.assertTrue(taxinfosection.verifyTaxInformationSection(), "verifyTaxInformation method failed");
+		Assert.assertTrue(taxinfosection.verifyHelpLinks(), "VerifyHelpLinks method failed");
+
 	}
-	
+
 	@Override
-	public TGLConstants getConstants()	{
+	public TGLConstants getConstants() {
 		return new TGLConstants();
 	}
-	
-	
+
 }
