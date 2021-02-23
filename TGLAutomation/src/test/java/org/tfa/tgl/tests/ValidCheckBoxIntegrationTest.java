c219e3a8b694349953f03da9b02735e6fda8003f
Nitin <nitin.sharma@teachforamerica.org>
2021-02-23 14:57:49 +0530

    Revert "updated ValidCheckBoxIntegrationTest"
    This reverts commit 91866cb23b4b9c769eae00bdd97517c5c247408e.

--- TGLAutomation/src/test/java/org/tfa/tgl/tests/ValidCheckBoxIntegrationTest.java
+++ TGLAutomation/src/test/java/org/tfa/tgl/tests/ValidCheckBoxIntegrationTest.java
@@ -16,7 +16,6 @@ import org.tfa.framework.utilities.testdata.TestData;
 import org.tfa.tgl.pages.ApplicantCenterPage;
 import org.tfa.tgl.pages.LoginPageAppCenter;
 import org.tfa.tgl.pages.LoginPageTgl;
-import org.tfa.tgl.pages.SearchDetailsPageTGL;
 import org.tfa.tgl.pages.SearchPageTGL;
 import org.tfa.tgl.utilities.web.TGLConstants;
 import org.tfa.tgl.utilities.web.TGLWebUtil;
@@ -36,7 +35,6 @@ public class ValidCheckBoxIntegrationTest extends BaseTestMethods{
 	private SearchPageTGL searchPage= new SearchPageTGL();
 	private ApplicantCenterPage applicantCenterPage = new ApplicantCenterPage();
 	private LoginPageAppCenter loginAppCenter=new LoginPageAppCenter();
-	private SearchDetailsPageTGL searchDetailsPage= new SearchDetailsPageTGL();
 	private  Map<String, String> infoMap; 
 	private RandomUtil random=new RandomUtil();
 	private JavaScriptUtil jsUtil=JavaScriptUtil.getObject();
@@ -73,8 +71,6 @@ public class ValidCheckBoxIntegrationTest extends BaseTestMethods{
 		searchPage.clickOnSearchBtn();
 		searchPage.clickFirstRowColumnOnSearchResults();
 		jsUtil.scrollDownPage(500);
-		searchDetailsPage.selectTGLStatusDD("Incomplete");
-
 
 		/* 
 		* Step 4 - 		Click Valid check box  for any of the available doc type which has required check box checked 
