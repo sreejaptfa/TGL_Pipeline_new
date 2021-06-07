2835f71ec92ff0fd56c2cb2cbab5f55ddf7413f9
csuryaprasad <csuryaprasad@gmail.com>
2021-06-07 09:22:06 -0400

    CSV file convertion

---
+++ TGLAutomation/src/test/java/org/tfa/tgl/tests/ExcelToCsvConvert.java
@@ -0,0 +1,12 @@
+package org.tfa.tgl.tests;
+
+import org.tfa.framework.utilities.testdata.ExcelToCsvConvertor;
+
+public class ExcelToCsvConvert {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+       ExcelToCsvConvertor.convert();
+	}
+
+}
